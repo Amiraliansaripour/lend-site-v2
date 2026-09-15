6e2606f63e0d9edd212839ad879b22b4a9226195
amiraliansaripour <amiraliansaripor1383@gmail.com>
2026-09-15 17:03:56 +0330

    refactor app sidebar to use unread message count and update badge handling; clean up messages page component by removing unused code and imports

--- lib/api/client/index.ts
+++ lib/api/client/index.ts
@@ -67,7 +67,8 @@ const $fetch = async <P, D>(url: string, options?: $FetchOptions<P>) => {
         pathname.includes('/wallets') ||
         pathname.includes('/installments') ||
         pathname.includes('/profile') ||
-        pathname.includes('/cards');
+        pathname.includes('/cards') ||
+        pathname.includes('/messages');
 
       if (isDashboardLikeRoute) {
         window.location.href = '/';
